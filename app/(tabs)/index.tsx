1715473c42b429eab463cf5d0a5e8a98e179e601
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2025-09-18 18:34:50 +0200

    Made darkmode work

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import ColorBlock from "@/components/ColorBlock";
 import tw from "@/lib/tailwind";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View style={tw`flex-1 justify-center items-center`}>
+    <View style={tw`flex-1 justify-center items-center dark:bg-black`}>
       <Text style={tw`mb-2`}>Edit app/index.tsx to edit this screen.</Text>
       <ColorBlock twColor="bg-cyan-600" />
 
