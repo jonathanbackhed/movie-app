b4ef9e59470b486abe954d2d07032d5da58f1e29
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2025-09-22 05:00:24 +0200

    Refactor recommendation interfaces to use shared media types

--- interfaces/recommendations.ts
+++ interfaces/recommendations.ts
@@ -1,32 +1,6 @@
-interface BaseRecommendation {
-  adult: boolean;
-  backdrop_path: string | null;
-  id: number;
-  overview: string;
-  poster_path: string | null;
-  media_type: string;
-  original_language: string;
-  genre_ids: number[];
-  popularity: number;
-  vote_average: number;
-  vote_count: number;
-}
+import { MovieShort, SeriesShort } from "./shared";
 
-interface MovieRecommendation extends BaseRecommendation {
-  title: string;
-  original_title: string;
-  release_date: string;
-  video: boolean;
-}
-
-interface SeriesRecommendation extends BaseRecommendation {
-  name: string;
-  original_name: string;
-  first_air_date: string;
-  origin_country: string[];
-}
-
-export type Recommendation = MovieRecommendation | SeriesRecommendation;
+export type Recommendation = MovieShort | SeriesShort;
 
 export interface Recommendations {
   page: number;
--- interfaces/shared.ts
+++ interfaces/shared.ts
@@ -20,3 +20,33 @@ export interface SpokenLanguage {
   iso_639_1: string;
   name: string;
 }
+
+export interface BaseShortMedia {
+  adult: boolean;
+  backdrop_path: string | null;
+  id: number;
+  overview: string;
+  poster_path: string | null;
+  media_type: string;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieShort extends BaseShortMedia {
+  title: string;
+  original_title: string;
+  release_date: string;
+  video: boolean;
+}
+
+export interface SeriesShort extends BaseShortMedia {
+  name: string;
+  original_name: string;
+  first_air_date: string;
+  origin_country: string[];
+}
+
+export type MediaShort = MovieShort | SeriesShort;
