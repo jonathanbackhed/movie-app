bd0d377b48fe4578c0856924e19f85937c4a3465
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2025-09-23 02:42:30 +0200

    Add duskMode support to Error and Loading screens

--- components/screens/ErrorScreen.tsx
+++ components/screens/ErrorScreen.tsx
@@ -2,14 +2,18 @@ import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "@/lib/tailwind";
+import { useSettingsStore } from "@/lib/hooks/useSettingsStore";
 
 interface Props {
   message?: string;
 }
 
 export default function ErrorScreen({ message = "Failed to load data" }: Props) {
+  const { duskMode } = useSettingsStore();
   return (
-    <SafeAreaView style={tw`flex-1 justify-center items-center bg-gray-100 dark:bg-black`}>
+    <SafeAreaView
+      style={tw`flex-1 justify-center items-center bg-gray-100 ${duskMode ? "dark:bg-zinc-900" : "dark:bg-black"}`}
+    >
       <Text style={tw`text-xl dark:text-white`}>{message}</Text>
     </SafeAreaView>
   );
--- components/screens/LoadingScreen.tsx
+++ components/screens/LoadingScreen.tsx
@@ -2,6 +2,7 @@ import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "@/lib/tailwind";
+import { useSettingsStore } from "@/lib/hooks/useSettingsStore";
 
 interface Props {
   message?: string;
@@ -9,8 +10,13 @@ interface Props {
 }
 
 export default function LoadingScreen({ message = "Loading...", center }: Props) {
+  const { duskMode } = useSettingsStore();
   return (
-    <SafeAreaView style={tw`flex-1 items-center bg-gray-100 dark:bg-black ${center ? "justify-center" : ""}`}>
+    <SafeAreaView
+      style={tw`flex-1 items-center bg-gray-100 ${duskMode ? "dark:bg-zinc-900" : "dark:bg-black"} ${
+        center ? "justify-center" : ""
+      }`}
+    >
       <Text style={tw`text-xl dark:text-white`}>{message}</Text>
     </SafeAreaView>
   );
