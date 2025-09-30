58f669c98eb800d17cf6a98e67251f318126410a
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2025-09-30 16:13:26 +0200

    Credits now clickable

--- components/media/PersonPreviewCard.tsx
+++ components/media/PersonPreviewCard.tsx
@@ -5,6 +5,7 @@ import { BASE_IMAGE_URL } from "@/constants/settings";
 import { ProfileSize } from "@/constants/enums";
 import tw from "@/lib/tailwind";
 import { GlassView } from "expo-glass-effect";
+import { Link } from "expo-router";
 
 interface Props {
   id: number;
@@ -14,8 +15,26 @@ interface Props {
 }
 
 export default function PersonPreviewCard({ id, name, role, profile_path }: Props) {
-  return (
-    <View style={tw`px-2 flex items-center`}>
+  return id ? (
+    <Link href={`/person/${id}`} style={tw``}>
+      <View style={tw`px-2 items-center`}>
+        {profile_path === null ? (
+          <GlassView style={tw`w-[70px] h-[70px] rounded-full bg-zinc-300`} glassEffectStyle="regular" />
+        ) : (
+          <Image
+            source={BASE_IMAGE_URL + ProfileSize.w185 + profile_path}
+            alt="profile image"
+            contentFit="cover"
+            style={tw`w-[70px] h-[70px] rounded-full`}
+          />
+        )}
+        {/* <View style={tw`w-[70px] h-[70px] rounded-full bg-zinc-300`}></View> */}
+        <Text style={tw`text-sm font-bold dark:text-white`}>{name}</Text>
+        <Text style={tw`text-xs dark:text-white`}>{role}</Text>
+      </View>
+    </Link>
+  ) : (
+    <View style={tw`px-2 items-center`}>
       {profile_path === null ? (
         <GlassView style={tw`w-[70px] h-[70px] rounded-full bg-zinc-300`} glassEffectStyle="regular" />
       ) : (
